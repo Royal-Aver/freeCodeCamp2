a02bfbaba5f2fe9969c2dc0f53f7ec140bc30842
Royal-Aver <royal_aver@rambler.ru>
2022-11-02 16:17:25 +0300

    added commit

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -147,11 +147,164 @@ result = twinkleStar.match(starRegex); //[ 'Twinkle', 'twinkle' ]
 let humStr = "I'll hum a song";
 let hugStr = "Bear hug";
 let huRegex = /hu./;
-console.log(huRegex.test(humStr)); //true
-console.log(huRegex.test(hugStr)); //true
+huRegex.test(humStr); //true
+huRegex.test(hugStr); //true
 
 // //Complete the regex unRegex so that it matches the strings run, sun, fun, pun, nun, and bun. Your regex should use the wildcard character.
 // Дополните регулярное выражение unRegex так, чтобы оно соответствовало строкам run, sun, fun, pun, nun и bun. Ваше регулярное выражение должно использовать подстановочный знак.
 let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/; // Change this line
 result = unRegex.test(exampleStr);
+
+
+
+//Match Single Character with Multiple Possibilities
+// Сопоставьте одного персонажа с несколькими возможностями
+
+// You learned how to match literal patterns (/literal/) and wildcard character (/./). Those are the extremes of regular expressions, where one finds exact matches and the other matches everything. There are options that are a balance between the two extremes.
+// Вы узнали, как сопоставлять литеральные шаблоны (/literal/) и подстановочные знаки (/./). Это крайности регулярных выражений, когда одно находит точные совпадения, а другое — все. Есть варианты, представляющие собой баланс между двумя крайностями.
+
+// You can search for a literal pattern with some flexibility with character classes. Character classes allow you to define a group of characters you wish to match by placing them inside square ([ and ]) brackets.
+// Вы можете искать буквальный шаблон с некоторой гибкостью с помощью классов символов. Классы символов позволяют вам определить группу символов, которые вы хотите сопоставить, поместив их в квадратные ([ и ]) скобки.
+
+//For example, you want to match bag, big, and bug but not bog. You can create the regex /b[aiu]g/ to do this. The [aiu] is the character class that will only match the characters a, i, or u.
+// Например, вы хотите сопоставить bag, big, и bug but но не bog. Для этого вы можете создать регулярное выражение /b[aiu]g/. [aiu] — это класс символов, который будет соответствовать только символам a, i или u.
+
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex); //["big"]
+bagStr.match(bgRegex); //["bag"]
+bugStr.match(bgRegex); //["bug"]
+bogStr.match(bgRegex); //null
+
+// Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample. Note: Be sure to match both upper- and lowercase vowels.
+// Используйте класс символов с гласными (a, e, i, o, u) в вашем регулярном выражении vowelRegex, чтобы найти все гласные в строке quoteSample. Примечание. Убедитесь, что гласные в верхнем и нижнем регистре совпадают.
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/ig;
+result = quoteSample.match(vowelRegex); //[  'e', 'a', 'e', 'o', 'u',  'i', 'e', 'a', 'o',  'e', 'o', 'e',  'I', 'a', 'e',  'o',  'o', 'e',  'i', 'o', 'e',  'o',  'i', 'e', 'i'] нашел все буквы несмотря на регистр
+
+
+
+// Match Letters of the Alphabet
+//Совпадение букв алфавита
+
+// You saw how you can use character sets to specify a group of characters to match, but that's a lot of typing when you need to match a large range of characters (for example, every letter in the alphabet). Fortunately, there is a built-in feature that makes this short and simple.
+// Вы видели, как можно использовать наборы символов, чтобы указать группу символов для сопоставления, но это требует много времени, когда вам нужно сопоставить большой диапазон символов (например, каждую букву алфавита). К счастью, есть встроенная функция, которая делает это коротким и простым.
+
+// Inside a character set, you can define a range of characters to match using a hyphen character: -.
+// Внутри набора символов вы можете определить диапазон символов для сопоставления с помощью символа дефиса: -.
+
+// For example, to match lowercase letters a through e you would use [a-e].
+// Например, чтобы найти строчные буквы от a до e, вы должны использовать [a-e].
+let catStr = "cat";
+let batStr = "bat";
+let matStr = "mat";
+bgRegex = /[a-e]at/;
+catStr.match(bgRegex); //["cat"]
+batStr.match(bgRegex); //["bat"]
+matStr.match(bgRegex); //null
+
+// Match all the letters in the string quoteSample.
+// Note: Be sure to match both uppercase and lowercase letters.
+// Совпадение со всеми буквами в строке quoteSample. Примечание. Убедитесь, что совпадают как прописные, так и строчные буквы.
+quoteSample = "The quick brown fox jumps over the lazy dog.";
+alphabetRegex = /[a-z]/gi;
+result = quoteSample.match(alphabetRegex);
+
+
+
+// Match Numbers and Letters of the Alphabet
+// Совпадение чисел и букв алфавита
+
+// Using the hyphen (-) to match a range of characters is not limited to letters. It also works to match a range of numbers.
+// Использование дефиса (-) для соответствия диапазону символов не ограничивается буквами. Он также работает, чтобы соответствовать диапазону чисел.
+
+// For example, /[0-5]/ matches any number between 0 and 5, including the 0 and 5.
+// Например, /[0-5]/ соответствует любому числу от 0 до 5, включая 0 и 5.
+
+// Also, it is possible to combine a range of letters and numbers in a single character set.
+// Кроме того, можно комбинировать диапазон букв и цифр в одном наборе символов.
+let jennyStr = "Jenny8675309";
+myRegex = /[a-z0-9]/ig;
+jennyStr.match(myRegex);
+
+// Create a single regex that matches a range of letters between h and s, and a range of numbers between 2 and 6. Remember to include the appropriate flags in the regex.
+// Создайте одно регулярное выражение, которое соответствует диапазону букв от h до s и диапазону чисел от 2 до 6. Не забудьте включить в регулярное выражение соответствующие флаги.
+quoteSample = "Blueberry 3.141592653s are delicious.";
+myRegex = /[h-s2-6]/gi;
+result = quoteSample.match(myRegex);
+
+
+// Match Single Characters Not Specified
+// Совпадение с отдельными символами, не указанными
+
+// So far, you have created a set of characters that you want to match, but you could also create a set of characters that you do not want to match. These types of character sets are called negated character sets.
+// До сих пор вы создали набор символов, которые вы хотите сопоставить, но вы также можете создать набор символов, которые вы не хотите сопоставлять. Эти типы наборов символов называются наборами символов с отрицанием.
+
+// To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
+// Чтобы создать отрицательный набор символов, вы помещаете символ вставки (^) после открывающей скобки и перед символами, которые не должны совпадать.
+
+//For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters.
+// Например, /[^aeiou]/gi соответствует всем символам, не являющимся гласными. Обратите внимание, что такие символы, как ., !, [, @, / и пробел, совпадают — набор символов с отрицанием гласных исключает только символы гласных.
+
+// Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
+// Создайте одно регулярное выражение, которое соответствует всем символам, не являющимся числом или гласной. Не забудьте включить соответствующие флаги в регулярное выражение.
+
+quoteSample = "3 blind mice.";
+myRegex = /[^aeiou^0-9]/gi;
+result = quoteSample.match(myRegex);
+
+
+
+//Match Characters that Occur One or More Times
+// Сопоставьте символы, которые встречаются один или несколько раз
+
+// Sometimes, you need to match a character (or group of characters) that appears one or more times in a row. This means it occurs at least once, and may be repeated.
+// Иногда вам нужно сопоставить символ (или группу символов), который появляется один или несколько раз подряд. Это означает, что это происходит хотя бы один раз и может повторяться.
+
+//You can use the + character to check if that is the case. Remember, the character or pattern has to be present consecutively. That is, the character has to repeat one after the other.
+// Вы можете использовать символ +, чтобы проверить, так ли это. Помните, что символ или шаблон должны присутствовать последовательно. То есть символ должен повторяться один за другим.
+
+// For example, /a+/g would find one match in abc and return ["a"]. Because of the +, it would also find a single match in aabc and return ["aa"].
+// Например, /a+/g найдет одно совпадение в abc и вернет ["a"]. Из-за + он также найдет одно совпадение в aabc и вернет ["aa"].
+
+//If it were instead checking the string abab, it would find two matches and return ["a", "a"] because the a characters are not in a row - there is a b between them. Finally, since there is no a in the string bcd, it wouldn't find a match.
+// Если бы вместо этого проверялась строка abab, она нашла бы два совпадения и вернула бы ["a", "a"], потому что символы a не стоят в строке - между ними есть b. Наконец, поскольку в строке bcd нет символа a, совпадение не будет найдено.
+
+//You want to find matches when the letter s occurs one or more times in Mississippi. Write a regex that uses the + sign.
+// Вы хотите найти совпадения, когда буква s встречается один или несколько раз в Миссисипи. Напишите регулярное выражение, использующее знак +.
+let difficultSpelling = "Misssissippi";
+myRegex = /s+/g;
+result = difficultSpelling.match(myRegex); //[ 'sss', 'ss' ]
+
+
+
+// Match Characters that Occur Zero or More Times
+// Соответствие символам, которые встречаются ноль или более раз
+
+// The last challenge used the plus + sign to look for characters that occur one or more times. There's also an option that matches characters that occur zero or more times.
+// В последней задаче использовался знак плюс + для поиска символов, которые встречаются один или несколько раз. Существует также параметр, который соответствует символам, встречающимся ноль или более раз.
+
+// The character to do this is the asterisk or star: *.
+// Символ для этого — звездочка или звездочка: *.
+
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+soccerWord.match(goRegex); //["goooooooo"]
+gPhrase.match(goRegex); //["g"]
+oPhrase.match(goRegex); //null
+
+//For this challenge, chewieQuote has been initialized as the string Aaaaaaaaaaaaaaaarrrgh! behind the scenes. Create a regex chewieRegex that uses the * character to match an uppercase A character immediately followed by zero or more lowercase a characters in chewieQuote. Your regex does not need flags or character classes, and it should not match any of the other quotes.
+// Для этого задания ChewieQuote был инициализирован строкой Aaaaaaaaaaaaaaarrrgh! за кулисами. Создайте регулярное выражение ChewieRegex, которое использует символ * для сопоставления с символом A в верхнем регистре, за которым непосредственно следует ноль или более символов a нижнего регистра в ChewieQuote. Вашему регулярному выражению не нужны флаги или классы символов, и оно не должно совпадать ни с какими другими кавычками.
+
+let chewieQuote = 'He made a fair move. Screaming about it can\'t help you.'
+let chewieRegex = /Aa*/; // Change this line
+// Only change code above this line
+
+result = chewieQuote.match(chewieRegex);
