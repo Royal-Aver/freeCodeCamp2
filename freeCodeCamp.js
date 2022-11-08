3be3b2c923f6d473f8db73d8e2df41c0251f5ae4
Royal-Aver <royal_aver@rambler.ru>
2022-11-08 09:41:29 +0300

    added easy example

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -925,4 +925,21 @@ function toWeirdCase(string){
     return res = arr.slice(0, -1)
 }
 
-console.log(toWeirdCase('This is a test')) //'ThIs Is A TeSt
+toWeirdCase('This is a test') //'ThIs Is A TeSt
+
+
+
+//Напишите функцию bmi, которая вычисляет индекс массы тела (bmi = вес / рост * 2).
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+function bmi(weight, height) {
+  const bodyMassIndex = weight / (height **2);
+  if (bodyMassIndex <= 18.5) return "Underweight";
+  else if (bodyMassIndex <= 25) return "Normal";
+  else if (bodyMassIndex <= 30) return "Overweight";
+  else return "Obese";
+}
+
+console.log(bmi(90, 1.80)) //"Normal")
